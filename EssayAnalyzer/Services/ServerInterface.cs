31de3dbb1128d38adce9be34439c60ef44cf1075
jaebanks <jared.ebanks@gmail.com>
2014-04-15 22:41:41 -0500

    Frequency/Proximity/Tests Completed

--- EssayAnalyzer/Services/ServerInterface.cs
+++ EssayAnalyzer/Services/ServerInterface.cs
@@ -43,71 +43,8 @@ namespace Services
             string[] wordArray = StringToWordLister.StringToWordArray(_essay.UploadedText);
             _essay.Analysis.TotalWordCount = WordCounter.WordCount(wordArray);
             _essay.Analysis.IndividualWordFrequency = WordUsageCounter.WordUsageCount(wordArray);
-        }
-
-
-
-        /*
-        /// <summary>
-        /// TODO: XML Comment
-        /// </summary>
-<<<<<<< HEAD
-=======
-        private void WordUsage(string[] wordList)
-        {
-            foreach (string word in wordList)
-            {
-                if (WordUsage.ContainsKey(word))
-                {
-                    WordUsage[word] = WordUsage[word] + 1;
-                }
-                else
-                {
-                    WordUsage.Add(word, 1);
-                }
-            }
-            // TODO: Directly access the analysis.IndividualWordFrequency.
-            // No need for unneeded variables. 
-            analysis.WordFrequency = WordUsage;
-        }*/
-
-        /// <summary>
-        /// Adds high frequency words to the HighFrequencyWords dictionary.
-        /// Individual words that account for 2% or more of the total word count are considered high-frequency.
-        /// </summary>
-        private void HighFrequencyWords()
-        {
-            double highFrequencyValue = (_essay.Analysis.TotalWordCount * .02);
-
-            foreach (var pair in _essay.Analysis.IndividualWordFrequency)
-            {
-                if (pair.Value >= highFrequencyValue)
-                    _essay.Analysis.HighFrequencyWordList.Add(pair.Key);
-            }
-        }
-
-        
-        /// <summary>
-        /// Analyzes how close each word in the given list is to each other.
-        /// </summary>
-        /// <param name="wordList">
-        /// Ordered list of words to be analyzed.
-        /// </param>
-        private void ProximityWordAnalysis(string[] wordList)
-        {      
-            int wordNumber = 0;
-            foreach (string word in wordList)
-            {
-                wordNumber += 1;
-                if (_essay.Analysis.IndividualWordProximity.ContainsKey(word))
-                {
-                    _essay.Analysis.IndividualWordProximity[word] = new int[] { (_essay.Analysis.IndividualWordProximity[word][1] + (1 * 1 / (wordNumber - _essay.Analysis.IndividualWordProximity[word][2]))), wordNumber };
-                }
-                else
-                {
-                    _essay.Analysis.IndividualWordProximity.Add(word, new int[] { 1, wordNumber });
-                }
-            }
+            _essay.Analysis.HighFrequencyWordList = HighFrequencyWordLister.HighFrequencyWords(wordArray);
+            _essay.Analysis.CloseProximityWordList = CloseProximityWordLister.ProximityWordAnalysis(wordArray);
         }
 
     }
